24dab4ba1c22e81a38b2ed9bbc38ee57ba6279ba
Julia Popp <juliampopp@gmail.com>
2022-11-11 17:41:59 -0800

    commit tree 96ae115420a5f789485712db56edfbb3a38a10c9

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Gojo Satoru",
+    streetAddress: "123 Jujutsu St",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeDelete = {...employee};
+    delete employeeDelete[key];
+    return employeeDelete;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
